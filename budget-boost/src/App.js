e75b06e5283279d11a1f6a7e3f67401a75bb1246
Rosa Li <rosali@rosas-mbp.wireless.concordia.ca>
2023-01-21 22:50:57 -0500

    made more changes

--- budget-boost/src/App.js
+++ budget-boost/src/App.js
@@ -3,12 +3,14 @@ import Expense from './components/Expense';
 import Income from './components/Income';
 import Navbar from './components/Navbar';
 import Topbar from './components/Topbar';
+import Transactions from './components/Transaction';
 
 function App() {
   return (
     <div>
       <Topbar />
       <Expense />
+      <Transactions />
       {/* <Income /> */}
       {/* <Navbar />  */}
     </div>
